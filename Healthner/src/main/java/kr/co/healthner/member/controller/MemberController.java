8dc0701d10b758eca52666dc0f69f18f3f62b361
Park.T.G <xorb5693@naver.com>
2020-06-22 11:32:10 +0900

    tg Attendance Commit

--- Healthner/src/main/java/kr/co/healthner/member/controller/MemberController.java
+++ Healthner/src/main/java/kr/co/healthner/member/controller/MemberController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.co.healthner.common.CardHandler;
 import kr.co.healthner.member.model.service.MemberServiceImpl;
+import kr.co.healthner.member.model.vo.AttendanceData;
 
 @Controller
 @RequestMapping("/healthner/member")
@@ -13,6 +16,10 @@ public class MemberController {
 	@Autowired
 	@Qualifier("memberService")
 	private MemberServiceImpl service;
+	
+	@Autowired
+	@Qualifier("cardHandler")
+	private CardHandler cardHandler;
 	
 	@RequestMapping("/loginFrm.do")
 	public String loginFrm() {
@@ -23,5 +30,35 @@ public class MemberController {
 	public String registerFrm() {
 		return "member/registerFrm";
 	}
+
+	@ResponseBody
+	@RequestMapping("/arduinoInsert.do")
+	public String arduinoInsert(String card) {
+		card.replace(" ", "");
+		String cardNo = card.replaceAll(" ", ".").substring(1);
+		System.out.println("Insert : " + cardNo);
+		try {
+			cardHandler.cardResponse(cardNo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "$" + cardNo;
+	}
 	
+	@ResponseBody
+	@RequestMapping("/arduinoAttendance.do")
+	public String arduinoAttendance(String card) {
+		card.replace(" ", "");
+		String cardNo = card.replaceAll(" ", ".").substring(1);
+		System.out.println("Attendance : " + cardNo);
+		AttendanceData data = service.aduinoAttendance(cardNo);
+		
+		if (data == null) {
+			return "$Find Not User";
+		} else if (data.getResult() > 0) {
+			return "$" + data.getMember().getMemberId();
+		} else {
+			return "$not found";
+		}
+	}
 }
--- Healthner/src/main/java/kr/co/healthner/member/model/dao/MemberDaoImpl.java
+++ Healthner/src/main/java/kr/co/healthner/member/model/dao/MemberDaoImpl.java
@@ -1,8 +1,25 @@
 package kr.co.healthner.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import kr.co.healthner.member.model.vo.Member;
 
 @Repository("memberDao")
 public class MemberDaoImpl {
+
+	@Autowired
+	SqlSessionTemplate sqlSession;
 	
+	public Member selectAduino(String cardNo) {
+		
+		return sqlSession.selectOne("member.selectAduino", cardNo);
+	}
+
+	public int aduinoAttendance(int memberNo) {
+		
+		return sqlSession.insert("attendance.insertAttendance", memberNo);
+	}
+
 }
