74f80e57e211450734140d62a564d56a04c8abee
xorb5693 <xorb5693@naver.com>
2020-06-26 11:20:27 +0900

    Trainer import commit

--- Healthner/src/main/java/kr/co/healthner/trainer/model/dao/TrainerDaoImpl.java
+++ Healthner/src/main/java/kr/co/healthner/trainer/model/dao/TrainerDaoImpl.java
@@ -1,7 +1,5 @@
 package kr.co.healthner.trainer.model.dao;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -9,13 +7,11 @@ import javax.servlet.http.HttpSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import kr.co.healthner.member.model.vo.Member;
 import kr.co.healthner.trainer.model.vo.BmiVO;
 import kr.co.healthner.trainer.model.vo.MemberMappingInfoVO;
 import kr.co.healthner.trainer.model.vo.TrainerVO;
-import kr.co.healthner.vo.MemberMappingVO;
 
 @Repository("trainerDao")
 public class TrainerDaoImpl {
